0dc97c965ea201601d48d4fca6aa63454d3c8b89
Manas <manas.codewritter@gmail.com>
2026-03-12 19:56:58 +0530

    store configure using rtk

--- Project/src/config/config.js
+++ Project/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apwriteUrl: String(import.meta.env.VITE_APPWRITE_UR),
+    apwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     apwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     apwriteDatabaseID: String(import.meta.env.VITE_APIWRITE_DATABASE_ID),
     apwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
